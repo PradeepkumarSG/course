916aece3f0950c36cc40d69256f15252212a2fd1
PradeepKumarSG <pradeepkumar.b@icloud.com>
2021-01-05 00:02:36 +0800

    new class

--- lecture-3/project01/tasks/views.py
+++ lecture-3/project01/tasks/views.py
@@ -1,4 +1,10 @@
+from django import forms
 from django.shortcuts import render
+from django.middleware import csrf
+
+class NewForm(forms.Form):
+  task = forms.CharField(label="New Task")
+  priorities = forms.IntegerField(label="priorities")
 
 tasks = ["read", "write", "copy", "paste"]
 # Create your views here.
@@ -6,4 +12,4 @@ def index(request):
   return render(request, "tasks/index.html", {"tasks":tasks})
 
 def add(request):
-  return render(request, "tasks/add.html")
+  return render(request, "tasks/add.html", {"form":NewForm()})
